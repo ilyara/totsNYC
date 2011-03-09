498d67be90ffbaba2eed18349d4e5d228172a369
Apple User <appleuser@Apple-Users-Mac.local>
2011-03-09 00:24:30 -0500

    first commit

---
+++ totsnyc/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Totsnyc::Application.config.secret_token = 'b110a357e08a65e96618bc56fb3d8494a8dd705b62a6e85c312ebcd7248bdaf93d8745c6994fcd9b130942f8b5e692accc25c2794025d5d810a6efb0c3147936'
