8a3d52e47cdb4c8b3f8775909d3ab216ce4e625d
ilyara <ilya@trinix.com>
2011-05-27 16:45:37 -0400

    works ok

---
+++ python/analyze.py
@@ -0,0 +1,43 @@
+"""
+Analyze data structure of a json file
+with an eye on storing data in a relational DB
+"""
+import json
+
+file = '/tmp/crunch/crunch/mahalo'
+page = open(file, 'r')
+print "loading record..."
+c = json.loads(page.read())
+print "loaded. Top-level dictionary contains %d keys" % len(c)
+# print c.keys()
+
+tables = {}
+objects = {}
+
+def walk(x, level=0, key='topLevel', parent_type=None):
+  indent = "\t" * level
+  if isinstance(x, dict):
+    if parent_type == dict:
+      print "%sobject(of type %s): %d keys" % (indent, key, len(x.keys()))	
+      objects[key] = x.keys()
+    else:
+      print "%srecord(in %s): %d keys" % (indent, key, len(x.keys()))
+      tables[key] = x.keys()
+    [walk(x[k], level+1, k, dict) for k in x.keys()]
+  elif isinstance(x, list):
+    print "%sreference table(%s): %d items" % (indent, key, len(x))
+    [walk(i, level+1, key, list) for i in x]
+	#   elif isinstance(x, unicode):
+	# print "%s:%s" % (key, x)
+  else:
+	print "%s%s(%s):%s" % (indent, key, type(x).__name__, x)
+	
+walk(c)
+
+print "====Tables:"
+for table in tables.keys():
+  print "%s:%s" % (table, ', '.join(tables[table]))
+
+print "====Objects:"
+for obj in objects.keys():
+  print "%s:%s" % (obj, ', '.join(objects[obj]))
--- python/crunch.py
+++ python/crunch.py
@@ -39,6 +39,14 @@ class Crunch:
 		- do we have an existing DB?
 		- does it have queue management tables / correct schema version?
 		"""
+
+		oplist = [i.strip() for i in self.ops.split(',')] 
+		try:
+			options = gnu_getopt(sys.argv[1:], 'l:w:', oplist)
+			print options
+		except GetoptError, e:
+			print str(e)
+			exit(1)
 		
 		self.worker = '%d/%d' % (os.getuid(),os.getpid())
 		config = ConfigParser.ConfigParser()
@@ -51,8 +59,8 @@ class Crunch:
 		except ConfigParser.NoSectionError: # config file or section does not exist
 			pass 							# using default values
 
-		if sys.argv[1:2] and sys.argv[1:2][0] in [i.strip() for i in self.ops.split(',')]:
-			self.op = sys.argv[1:2][0] 
+		if options[0][0][1] in oplist:
+			opflag, self.op = options[0][0]
 			try:
 				db_file = os.path.join(crunch_out_dir, config.get(self.op, "DBFile"))
 				self.raw_data_dir = os.path.join(crunch_out_dir, config.get(self.op, "RawDataDir"))
@@ -60,6 +68,9 @@ class Crunch:
 				self.singular = config.get(self.op, "Singular")
 			except ConfigParser.NoSectionError: # config file or section does not exist
 				pass
+		else:
+			print "must specify a valid action:\n%s" % self.ops
+			exit(1)
 
 		print "Pricessing: %s" % self.op
 		print "Using DB: %s" % db_file
@@ -70,17 +81,22 @@ class Crunch:
 		
 		import mydb
 		self.db = mydb.MyDb(db_file)
-		self.db.debug_flag = False
+		self.db.debug_flag = True
 		self.create_api_tables()
 		self.admin_tasks()
 		iters = 0
-		load_completed = False
+		if opflag == '-w' and self.op != 'dryrun':
+			load_completed = True
+			print "activating worker mode"
+		else:
+			load_completed = False
+		
 		while load_completed:
 			iters = iters + 1
 			self.register_worker()
-			id, url = self.assign_job()[0]
+			id, url, keyref = self.assign_job()[0]
 			if url:
-				load_completed = self.execute_job("http://api.crunchbase.com/v/1/person/%s.js" % url, url)
+				load_completed = self.execute_job(url, keyref)
 				if load_completed:
 					s = 1
 					print "sleeping for %d seconds" % s
@@ -90,12 +106,12 @@ class Crunch:
 			else:
 				exit(1)
 		
-		if self.op != 'dryrun':
+		if opflag == '-l' and self.op not in ['dryrun', 'worker']:
 			print 'click'
 			url_mask = "http://api.crunchbase.com/v/1/%s" % self.singular + "/%s.js"
 			self.load_tc_list(self.list_file, url_mask)
 		else:
-			print 'crap'
+			print 'no data load requested'
 		
 		# os.getpid(), os.getuid(), os.uname()
 		# select * from mgmt_api where sess_lastupdate < datetime('now', '-10 seconds');
@@ -156,26 +172,26 @@ class Crunch:
 		self.db.execSQL(sql)
 		if self.db.cursor.rowcount:
 			# print 'Job assigned!'
-			sql = "SELECT id, url FROM mgmt_url WHERE status IS NULL AND worker = '%s'" % self.worker
+			sql = "SELECT id, url, keyref FROM mgmt_url WHERE status IS NULL AND worker = '%s'" % self.worker
 			return self.db.runQuery(sql)
 		else:
 			print 'nothing to do!'
-			return [(None, None)]
+			return [(None, None, None)]
 		pass
 		
 	def execute_job(self, url, keyref):
 		print "retrieving %s" % url
-		sql = "UPDATE mgmt_url SET load_start = datetime('now') WHERE status IS NULL AND url = '%s' AND worker = '%s'" % (keyref, self.worker)
+		sql = "UPDATE mgmt_url SET load_start = datetime('now') WHERE status IS NULL AND url = '%s' AND worker = '%s'" % (url, self.worker)
 		self.db.execSQL(sql)
 		if self.db.cursor.rowcount:
 			status, data = self.fetch_url(url, os.path.join(self.raw_data_dir, keyref))
 			# print "status: %s" % status
 			if status == "200 OK":
-				sql = "UPDATE mgmt_url SET load_completed = datetime('now'), status = '200 OK' WHERE url = '%s' AND worker = '%s'" % (keyref, self.worker)
+				sql = "UPDATE mgmt_url SET load_completed = datetime('now'), status = '200 OK' WHERE url = '%s' AND worker = '%s'" % (url, self.worker)
 				self.db.execSQL(sql)
 				return time.time()
 			else:
-				sql = "UPDATE mgmt_url SET load_completed = datetime('now'), status = '%s' WHERE url = '%s' AND worker = '%s'" % (status, keyref, self.worker)
+				sql = "UPDATE mgmt_url SET load_completed = datetime('now'), status = '%s' WHERE url = '%s' AND worker = '%s'" % (status, url, self.worker)
 				self.db.execSQL(sql)
 				return time.time()
 #			else:
@@ -183,7 +199,7 @@ class Crunch:
 #				return False
 
 	def fetch_url(self, url, filename):
-		# print "will save %s to %s" % (url, filename)
+		print "will save %s to %s" % (url, filename)
 
 		user_agent = 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2a1pre) Gecko/20090428 Firefox/3.6a1pre'
 		headers =  {'User-agent': user_agent, 'Accept-encoding': 'gzip', 'Referer': url}
