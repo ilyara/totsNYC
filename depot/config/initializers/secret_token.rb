5e4e92cf1969a5c830ff4a8c17989d590494ae6e
ilya <ilya@trinix.com>
2011-08-22 16:08:41 -0400

    Iter B

--- depot/config/initializers/secret_token.rb
+++ depot/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = 'a1b8f930775b7a149c982827dc8869d71c8721ef3c48dde7c7e6bea3c4219139ed6045f17765000ebd8897f5f3bfd6de0857768878a2e128ea743fd4bd511453'
+Depot::Application.config.secret_token = '1ddaeb582cb9575782a0fd91f3987e1efff657ef681f37dfa6586a64aaf1ab8d7afd2350a8d16dfc3eaaab461acf8bb52f0b61b998c1794138adaa69659c2cda'
--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110822180529) do
+ActiveRecord::Schema.define(:version => 20110822183129) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price",       :precision => 8, :scale => 2
+    t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
