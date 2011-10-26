037b8244f2c14199162b4696fc763ad4d6340501
ilyara <ilya@trinix.com>
2011-10-26 19:13:35 -0400

    starbucks - going home

--- rent/app/controllers/home_controller.rb
+++ rent/app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
     @viewings_today = true if Time.now.hour < 19
     @today = I18n.localize(Date.today, :format => :rakita)
     @tomorrow = I18n.t('date.day_names')[(Time.now + 1.day).wday]
-    @map_url = 'http://maps.googleapis.com/maps/api/staticmap?zoom=14&size=512x512&maptype=roadmap&markers=color:blue|label:S|40.702147,-74.015794&markers=color:green|label:G|40.711614,-74.012318&markers=color:red|color:red|label:C|40.718217,-73.998284&sensor=false'    
+    # @map_url = 'http://maps.googleapis.com/maps/api/staticmap?zoom=14&size=512x512&maptype=roadmap&markers=color:blue|label:S|40.702147,-74.015794&markers=color:green|label:G|40.711614,-74.012318&markers=color:red|color:red|label:C|40.718217,-73.998284&sensor=false'    
   end
 
 end
--- rent/app/helpers/application_helper.rb
+++ rent/app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def map_url
+    gmap_base = 'http://maps.googleapis.com/maps/api/staticmap?&size=512x512&maptype=roadmap&sensor=false'
+    marker_base = "&markers="
+    markers = Building.all.collect {|b| [b.latitude, b.longitude].join(',')} .join('|')
+    gmap_base + marker_base + markers
+  end
 end
