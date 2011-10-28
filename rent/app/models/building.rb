6fb6f60a0647651b3de97b19cef8383b8f9c7e62
Ilya Ravich <ilyaravich@Ilyas-MacBook-Air.local>
2011-10-28 17:25:58 -0400

    air

--- rent/app/models/building.rb
+++ rent/app/models/building.rb
@@ -1,7 +1,7 @@
 class Building < ActiveRecord::Base
   attr_accessible :address, :company #, :company_id
   belongs_to :company
-  
+  has_many :listings
   geocoded_by :address_geo
   after_validation :geocode, :if => :address_changed?
   
