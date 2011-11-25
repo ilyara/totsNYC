77f95d425279f031b7fefa6fbfda4acac549fd3b
Ilya Ravich <ilya@pitchrack.com>
2011-11-25 10:40:05 -0500

    pitchfork

--- nokogiri/delme9.rb
+++ nokogiri/delme9.rb
@@ -4,6 +4,27 @@ require 'time'
 
 CONTENT_DIR = '../../content/bits/'
 
+    create_sql = <<SQL
+        create table if not exists buildings 
+        (id integer primary key, ref_url string, address string, name string, description string,
+        neigborhood string, source string, status string, load_time datetime);
+        create unique index if not exists idx_ref_url_bldg on buildings(ref_url);
+SQL
+
+    create_sql_listings = <<SQL
+        create table if not exists listings 
+        (id integer primary key, ref_url string, address string, name string, description string,
+        neigborhood string, source string, status string, load_time datetime);
+        create unique index if not exists idx_ref_url_bldg on buildings(ref_url);
+SQL
+
+    create_sql_companies = <<SQL
+        create table if not exists companies 
+        (id integer primary key, ref_url string, address string, name string, description string,
+        neigborhood string, source string, status string, load_time datetime);
+        create unique index if not exists idx_ref_url_bldg on buildings(ref_url);
+SQL
+
 def knock(file_name)
   file = File.open(File.expand_path(CONTENT_DIR+file_name), "r")
   Nokogiri::HTML(file)
@@ -20,3 +41,27 @@ a.each do |r|
   puts e[1].text # children.select {|e| not e.element?}
   print "=====\n"
 end
+
+# init db
+# if first_run
+#   create db structures
+#   seed db
+# session record 
+# for each buildings page
+#   get buildings page
+#   process each building
+#     create building if not exists
+# for each building detail page 
+#   process
+#     attributes
+#       link_or_create neighborhood
+#       link_or_create leasing manager
+#       decode structure
+#     description
+#     building amenities & services
+#     apartment features and amenities
+#     comments
+#     current listings
+# for each [new] current listing
+#   get listing
+#   process attributes
---
+++ pitchfork/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pitchfork::Application.config.secret_token = '36d8e3acbc7be6e2ba788ea7d3c4a1939b5f84e0742a42cd987c7403f1c53cdfec9c9ec7ba9a495237873637827b1c4561116e0a3e19e1e8aac2c93743a3539f'
