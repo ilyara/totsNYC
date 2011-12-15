669b55b8811e864e6e9148c0878d385dd145b9a1
ilya <ilya@trinix.com>
2011-12-14 19:15:09 -0500

    going home now

--- nokogiri/bits1.rb
+++ nokogiri/bits1.rb
@@ -8,7 +8,7 @@ CONTENT_DIR = '../../content/bits/'
 DB_NAME = "bitsdb.sqlite"
 BASE_URL = 'http://www.nybits.com/apartments'
 BUILDING_TYPE = %w(apartment_buildings condo_buildings coop_buildings)
-AREA = %w(downtown_manhattan midtown_manhattan uptown_manhattan upper_manhattan)
+AREA = %w(midtown_manhattan uptown_manhattan upper_manhattan) #downtown_manhattan
 
     create_sql_buildings = <<SQL
         create table if not exists buildings 
@@ -36,44 +36,61 @@ SQL
 
 # File.open(File.join(file_dir, cl_id), "w") {|f| f.write "hello\n"}
 
-mydb = MyDB.new(File.join(@file_dir, DB_NAME), create_sql_buildings)
+@mydb = MyDB.new(File.join(@file_dir, DB_NAME), create_sql_buildings)
 
 def knock(file_name)
   file = File.open(File.join(@file_dir, file_name), "r")
   Nokogiri::HTML(file)
 end
 
-file_name = ARGV[0] || '1.html'
-neigborhood_type = "test N"
-ownership_type = "test O"
+def batch(file_name, neigborhood_type="test N", ownership_type="test O")
+  puts file_name
 
-bits = knock file_name
+  bits = knock file_name
 
-a = bits.at_css('h2').next.css('li.standardli, li.shadedli') # [125..126]
+  a = bits.at_css('h2').next.css('li.standardli, li.shadedli') # [125..126]
 
-# puts a
-fields = %w(name ref_url address neigborhood description neigborhood_type ownership_type)
-records = []
-a.each do |e|
-#  print "\n=====\n"
-  name = e.css('a')[0].text
-  href = e.css('a')[0]['href']
-  address = e.at_css('a').next.text.strip[1...-1]; address = name if address.empty?
-  neigborhood = e.css('a')[1].text
-  description = e.at_css('br').next.text.strip.squeeze(" ") if not e.at_css('br').next.nil?
-  records.push [name, href, address, neigborhood, description, neigborhood_type, ownership_type]
-#  print "name: #{name}\nhref: #{href}\naddress: #{address}\nneigborhood: #{neigborhood}\ndescription: #{description}\n"
+  # puts a
+  fields = %w(name ref_url address neigborhood description neigborhood_type ownership_type)
+  records = []
+  a.each do |e|
+    begin
+  #  print "\n=====\n"
+  #  print "#{records.count}, "
+    name = e.css('a')[0].text
+    href = e.css('a')[0]['href']
+    if e.at_css('a').next.text[1] == "("
+      address = e.at_css('a').next.text.strip[1...-1]
+      neigborhood = e.css('a')[1].text
+    end
+    address = name if (address.nil? or address.empty?) and name[0].to_i > 0
+    description = e.at_css('br').next.text.strip.squeeze(" ") if not e.at_css('br').next.nil?
+    records.push [name, href, address, neigborhood, description, neigborhood_type, ownership_type]
+    # print "name: #{name}\nhref: #{href}\naddress: #{address}\nneigborhood: #{neigborhood}\ndescription: #{description}\n"
+    rescue
+      puts "problem in #{records.count} (#{name})"
+      puts e      
+    end
+  end
+
+  @mydb.bulk_insert "buildings", fields, records
+  puts "Processed #{records.count} records"
 end
 
-mydb.bulk_insert "buildings", fields, records
-puts "Processed #{records.count} records"
+#file_name = ARGV[0] || '1.html'
+#batch(file_name)
+
 area_building_type_file = []
 BUILDING_TYPE.each do |b|
   AREA.each do |a|
-    area_building_type_file.push "#{a}_#{b}.html"
+    batch "#{a}_#{b}.html", a, b
+#    area_building_type_file.push "#{a}_#{b}.html"
 #    puts "curl #{BASE_URL}/#{a}_#{b}.html > #{a}_#{b}.html"
   end
 end
+
+
+
 # init db
 # if first_run
 #   create db structures
